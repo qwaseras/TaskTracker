87adf2fac944431b558f02d4e6aaea6ee1a68b2a
qwaseras <qwqwqaqs@gmail.com>
2017-08-22 18:29:07 +0300

    configured mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,19 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'qwqwqaqs@gmail.com',
+    password:       'qwaserasqw',
+    domain:         'localhost:3000',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
